67a2e180f7a5207a0bec0c0f150fe5d3f8aaa7d8
Tobias Kildetoft <tobias.kildetoft@gmail.com>
2024-05-07 19:31:53 +0200

    Updated to new interface for DataAccessor

--- SimpleSQLite/IDataAccessor.cs
+++ SimpleSQLite/IDataAccessor.cs
@@ -3,11 +3,15 @@
 public interface IDataAccessor
 {
     Task<T?> GetById<T>(int id) where T : IEntity, new();
-    Task<IList<T>> GetAllWhere<T>(ISpecification<T> specification) where T : IEntity, new();
-    Task<T> GetFirstOrDefaultWhere<T>(ISpecification<T> specification) where T : IEntity, new();
-    Task<T> GetFirstWhere<T>(ISpecification<T> specification) where T : IEntity, new();
 
     Task<T> Create<T>(T entity) where T : IEntity, new();
     Task Update<T>(T entity) where T : IEntity, new();
     Task Delete<T>(int id) where T : IEntity, new();
+
+    Task<IList<T>> Get<T>(IAllSpecification<T> specification) where T : IEntity, new();
+    Task<T> Get<T>(IFirstSpecification<T> specification) where T : IEntity, new();
+    Task<T?> Get<T>(IFirstOrDefaultSpecification<T> specification) where T : IEntity, new();
+    Task<IList<S>> Get<T, S>(ISelectAllSpecification<T, S> specification) where T : IEntity, new();
+    Task<S> Get<T, S>(ISelectFirstSpecification<T, S> specification) where T : IEntity, new();
+    Task<S?> Get<T, S>(ISelectFirstOrDefaultSpecification<T, S> specification) where T : IEntity, new();
 }
