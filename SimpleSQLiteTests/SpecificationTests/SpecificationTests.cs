d10619009402e8cde762987b24761f466e906701
Tobias Kildetoft <tobias.kildetoft@gmail.com>
2024-05-10 21:35:14 +0200

    Added more tests

--- SimpleSQLiteTests/SpecificationTests/SpecificationTests.cs
+++ SimpleSQLiteTests/SpecificationTests/SpecificationTests.cs
@@ -35,5 +35,101 @@ namespace SimpleSQLite.Tests.SpecificationTests
             Assert.That(result, Is.Not.Null);
             Assert.That(result.Name, Is.EqualTo(nameOfSoughtEntity));
         }
+
+        [Test]
+        public async Task ByUniqueName_ReturnsNullIfNotExist()
+        {
+            // Arrange
+            var entities = SampleEntityCreator.GetSampleEntities().ToList();
+            var uniqueNameSought = Guid.NewGuid().ToString();
+
+            var specification = new ByUniqueName(uniqueNameSought);
+
+            // Act
+            SampleEntity? result = null;
+            using (var mockAccess = new DataAccessMock<SampleEntity>(entities))
+            {
+                result = await mockAccess.DataAccessor.Get(specification);
+            }
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public async Task SecondAndThirdLargest_ReturnsTwoIfTheyExist()
+        {
+            // Arrange
+            var entities = SampleEntityCreator.GetSampleEntities().ToList();
+            var maxValue = entities.Max(x => x.SomeInt);
+            var ThirdLargestName = "ThirdLargest";
+            var SecondLargestName = "SecondLargest";
+            var LargestName = "Largest";
+            entities.Add(new SampleEntity { SomeInt = maxValue+1, Name = ThirdLargestName });
+            entities.Add(new SampleEntity { SomeInt = maxValue + 2, Name = SecondLargestName });
+            entities.Add(new SampleEntity { SomeInt = maxValue + 3, Name = LargestName });
+
+            var specification = new SecondAndThirdLargest();
+
+            // Act
+            IEnumerable<SampleEntity>? result = null;
+            using (var mockAccess = new DataAccessMock<SampleEntity>(entities))
+            {
+                result = await mockAccess.DataAccessor.Get(specification);
+            }
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result.First().Name, Is.EqualTo(SecondLargestName));
+            Assert.That(result.Skip(1).First().Name, Is.EqualTo(ThirdLargestName));
+        }
+
+        [Test]
+        public async Task SecondAndThirdLargest_ReturnsSecondLargestIfOnlyTwo()
+        {
+            // Arrange
+            var entities = new List<SampleEntity>();
+            var SecondLargestName = "SecondLargest";
+            var LargestName = "Largest";
+            entities.Add(new SampleEntity { SomeInt = 2, Name = SecondLargestName });
+            entities.Add(new SampleEntity { SomeInt = 3, Name = LargestName });
+
+            var specification = new SecondAndThirdLargest();
+
+            // Act
+            IEnumerable<SampleEntity>? result = null;
+            using (var mockAccess = new DataAccessMock<SampleEntity>(entities))
+            {
+                result = await mockAccess.DataAccessor.Get(specification);
+            }
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.First().Name, Is.EqualTo(SecondLargestName));
+        }
+
+        [Test]
+        public async Task SecondAndThirdLargest_ReturnsEmptyIfOnlyOne()
+        {
+            // Arrange
+            var entities = new List<SampleEntity>();
+            var LargestName = "Largest";
+            entities.Add(new SampleEntity { SomeInt = 3, Name = LargestName });
+
+            var specification = new SecondAndThirdLargest();
+
+            // Act
+            IEnumerable<SampleEntity>? result = null;
+            using (var mockAccess = new DataAccessMock<SampleEntity>(entities))
+            {
+                result = await mockAccess.DataAccessor.Get(specification);
+            }
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count(), Is.EqualTo(0));
+        }
     }
 }
