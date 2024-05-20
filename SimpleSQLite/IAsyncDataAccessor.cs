2e1af83f911880a14e005e1283c65d4a1dddec31
Tobias Kildetoft <tobias.kildetoft@gmail.com>
2024-05-20 13:48:48 +0200

    Added interface for async access

--- SimpleSQLite/IAsyncDataAccessor.cs
+++ SimpleSQLite/IAsyncDataAccessor.cs
@@ -1,46 +1,46 @@
 ﻿namespace Kildetoft.SimpleSQLite;
 
 /// <summary>
-/// Main interface for synchronously interacting with data
+/// Main interface for asynchronously interacting with data
 /// Should be bound using the extension for IServiceCollection
 /// </summary>
-public interface IDataAccessor
+public interface IAsyncDataAccessor
 {
     /// <summary>
     /// Get an item with the specified Id if one exists, and null otherwise
     /// </summary>
-    Task<T?> GetById<T>(int id) where T : IEntity, new();
+    Task<T?> GetByIdAsync<T>(int id) where T : IEntity, new();
 
     /// <summary>
     /// Create the chosen item
     /// Returns the created item, including the Id if this gets set by the database
     /// </summary>
-    Task<T> Create<T>(T entity) where T : IEntity, new();
+    Task<T> CreateAsync<T>(T entity) where T : IEntity, new();
 
     /// <summary>
     /// Updates the specified item, identified by Id
     /// </summary>
-    Task Update<T>(T entity) where T : IEntity, new();
+    Task UpdateAsync<T>(T entity) where T : IEntity, new();
 
     /// <summary>
     /// Deletes the item with the specified Id
     /// </summary>
-    Task Delete<T>(int id) where T : IEntity, new();
+    Task DeleteAsync<T>(int id) where T : IEntity, new();
 
     /// <summary>
     /// Get all items defined by the given ISpecification
     /// </summary>
-    Task<IList<T>> Get<T>(IAllSpecification<T> specification) where T : IEntity, new();
+    Task<IList<T>> GetAsync<T>(IAllSpecification<T> specification) where T : IEntity, new();
 
     /// <summary>
     /// Get the first item defined by the given specification
     /// Throws an exception if no such item exists
     /// </summary>
-    Task<T> Get<T>(IFirstSpecification<T> specification) where T : IEntity, new();
+    Task<T> GetAsync<T>(IFirstSpecification<T> specification) where T : IEntity, new();
 
     /// <summary>
     /// Get the first item defined by the given specification
     /// Returns null if no such item exists
     /// </summary>
-    Task<T?> Get<T>(IFirstOrDefaultSpecification<T> specification) where T : IEntity, new();
+    Task<T?> GetAsync<T>(IFirstOrDefaultSpecification<T> specification) where T : IEntity, new();
 }
