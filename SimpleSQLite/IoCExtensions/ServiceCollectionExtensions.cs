27e71cc9d4bc0ad798cae30ef0cb095c74b4dbb7
Tobias Kildetoft <tobias.kildetoft@gmail.com>
2024-05-24 20:08:18 +0200

    Added possibility to remove the package from the service collection, and optionally delete the database

--- SimpleSQLite/IoCExtensions/ServiceCollectionExtensions.cs
+++ SimpleSQLite/IoCExtensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Data;
 
 namespace Kildetoft.SimpleSQLite.IoC;
 
@@ -17,5 +18,38 @@ public static class ServiceCollectionExtensions
         collection.AddSingleton<IDataAccessor, DataAccessor>();
         collection.AddSingleton<IAsyncDataAccessor, AsyncDataAccessor>();
         return new ConnectionRegistration(collection);
+    }
+
+    /// <summary>
+    /// Removes the registration of the interfaces for SimpleSQLite and closes the database connection
+    /// Does not delete any database files
+    /// </summary>
+    /// <exception cref="ReadOnlyException">If the IServiceCollection is readonly</exception>
+    public static IServiceCollection RemoveSimpleSQLite(this IServiceCollection collection, bool deleteDatabase = false)
+    {
+        if (collection.IsReadOnly)
+        {
+            throw new ReadOnlyException($"{nameof(collection)} is read only");
+        }
+        collection.RemoveDataAccessor();
+        collection.RemoveAsyncDataAccessor();
+        DatabaseConnectionFactory.CloseConnections(deleteDatabase);
+        return collection;
+    }
+
+    private static void RemoveDataAccessor(this IServiceCollection collection)
+    {
+        if (collection.FirstOrDefault(x => x.ServiceType == typeof(IDataAccessor)) is { } registration)
+        {
+            collection.Remove(registration);
+        }
+    }
+
+    private static void RemoveAsyncDataAccessor(this IServiceCollection collection)
+    {
+        if (collection.FirstOrDefault(x => x.ServiceType == typeof(IAsyncDataAccessor)) is { } registration)
+        {
+            collection.Remove(registration);
+        }
     }
 }
--- SimpleSQLite/Registration/DatabaseConnectionFactory.cs
+++ SimpleSQLite/Registration/DatabaseConnectionFactory.cs
@@ -5,30 +5,32 @@ namespace Kildetoft.SimpleSQLite.IoC;
 
 internal static class DatabaseConnectionFactory
 {
+    private static string? _connectionString;
     private static ISQLiteAsyncConnection? _asyncConnection;
     private static ISQLiteConnection? _connection;
 
     internal static ISQLiteAsyncConnection GetAsyncConnection()
     {
-        return _asyncConnection ?? throw new NotInitializedException("The async connection was accessed before being initialized");
+        return _asyncConnection ?? throw new NotInitializedException("The async connection was accessed before being initialized or after being closed");
     }
 
     internal static ISQLiteConnection GetConnection()
     {
-        return _connection ?? throw new NotInitializedException("The connection was accessed before being initialized");
+        return _connection ?? throw new NotInitializedException("The connection was accessed before being initialized or after being closed");
     }
 
     internal static void Initialize(string connectionString)
     {
-        _asyncConnection = new SQLiteAsyncConnection(connectionString);
-        _connection = new SQLiteConnection(connectionString);
+        _connectionString = connectionString;
+        _asyncConnection = new SQLiteAsyncConnection(_connectionString);
+        _connection = new SQLiteConnection(_connectionString);
     }
 
     internal static void AddTables(IEnumerable<Type> entityTypes)
     {
         if (_connection == null)
         {
-            throw new InvalidOperationException("Cannot add tables before initializing the connection or after starting to use the connection");
+            throw new InvalidOperationException("Cannot add tables before initializing the connection");
         }
         foreach (var type in entityTypes)
         {
@@ -43,5 +45,37 @@ internal static class DatabaseConnectionFactory
             throw new InvalidOperationException("Cannot add indexes before initializing the connection or after starting to use the connection");
         }
         _connection.CreateIndex(tableName, attributeName, unique);
+    }
+
+    internal static void CloseConnections(bool deleteDatabase = false)
+    {
+        CloseAsyncConnection();
+        CloseSyncConnection();
+
+        if (deleteDatabase)
+        {
+            DeleteDatabase();
+        }
+    }
+
+    private static void CloseSyncConnection()
+    {
+        _connection?.Close();
+        _connection = null;
+    }
+
+    private static void CloseAsyncConnection()
+    {
+        _asyncConnection?.CloseAsync();
+        _asyncConnection = null;
+    }
+
+    private static void DeleteDatabase()
+    {
+        if (string.IsNullOrEmpty(_connectionString))
+        {
+            return;
+        }
+        File.Delete(_connectionString);
     }
 }
