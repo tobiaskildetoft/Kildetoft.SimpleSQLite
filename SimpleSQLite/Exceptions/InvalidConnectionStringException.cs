19de2e034709b008aa13c2945d9fd647f1a6a457
Tobias Kildetoft <tobias.kildetoft@gmail.com>
2024-06-06 16:56:21 +0200

    Add new exception for problems with the provided connection string

---
+++ SimpleSQLite/Exceptions/InvalidConnectionStringException.cs
@@ -0,0 +1,7 @@
+﻿namespace Kildetoft.SimpleSQLite.Exceptions;
+
+public class InvalidConnectionStringException : Exception
+{
+    public InvalidConnectionStringException(string message) : base(message) { }
+    public InvalidConnectionStringException(string message, Exception innerException) : base(message, innerException) { }
+}
