8f2fb4d7ddcb83d92c4a409c41dd9f50a3a8129f
Tobias Kildetoft <tobias.kildetoft@gmail.com>
2024-06-06 17:18:33 +0200

    Add object for defining indexes

---
+++ SimpleSQLite/ReflectionHelpers/IndexDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace Kildetoft.SimpleSQLite.ReflectionHelpers;
+
+internal class IndexDefinition
+{
+    internal string? TableName { get; set; }
+    internal string? AttributeName { get; set; }
+    internal bool Unique { get; set; } 
+}
