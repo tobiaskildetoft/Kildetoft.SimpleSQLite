ccdb5ee30136983cc4998428ee934bf8639c8fc7
Tobias Kildetoft <tobias.kildetoft@gmail.com>
2024-06-06 18:05:07 +0200

    Move methods away

--- SimpleSQLite/Registration/ConnectionRegistration.cs
+++ SimpleSQLite/Registration/ConnectionRegistration.cs
@@ -1,9 +1,6 @@
 ﻿using Kildetoft.SimpleSQLite.Exceptions;
 using Kildetoft.SimpleSQLite.ReflectionHelpers;
 using Microsoft.Extensions.DependencyInjection;
-using SQLite;
-using System.Linq.Expressions;
-using System.Reflection;
 
 namespace Kildetoft.SimpleSQLite.IoC;
 
@@ -69,52 +66,8 @@ internal class ConnectionRegistration : IConnectionRegistration
     {
         foreach (var indexType in indexTypes)
         {
-            if (!SQLiteIndexes.IsIndex(indexType))
-            {
-                throw new UnsupportedIndexImplementationException($"The supplied type {indexType.Name} does not implement IIndex<T> for an IEntity T");
-            }
-            var tableName = GetTableNameFromGenericIndexType(indexType);
-            var attributeName = GetAttributeNameFromIndexType(indexType);
-            var unique = GetUniqueFromIndexType(indexType);
-            DatabaseConnectionFactory.AddIndex(tableName, attributeName, unique);
+            DatabaseConnectionFactory.AddIndex(indexType);
         }
         return this;
-    }
-
-    private bool GetUniqueFromIndexType(Type indexType)
-    {
-        if (Activator.CreateInstance(indexType) is IIndex index)
-        {
-            return index.Unique;
-        }
-        throw new UnsupportedIndexImplementationException($"The supplied type {indexType.Name} could not be created as an IIndex");
-    }
-
-    private string GetTableNameFromGenericIndexType(Type indexType)
-    {
-        var entitytype = indexType.GetGenericArguments().First();
-        var tableAttribute = entitytype.GetCustomAttribute<TableAttribute>();
-
-        return tableAttribute!.Name;
-    }
-
-    private string GetAttributeNameFromIndexType(Type indexType)
-    {
-        var indexObject = Activator.CreateInstance(indexType);
-        var indexDefinitionProperty = indexType.GetProperty(IIndex.IndexDefinitionName);
-        if (indexDefinitionProperty?.GetValue(indexObject) is LambdaExpression expression)
-        {
-            return GetAttributeNameFromLambdaExpression(expression);
-        }
-        throw new UnsupportedIndexImplementationException($"The field {IIndex.IndexDefinitionName} on the type {indexType.Name} is not a LambdaExpression");
-    }
-
-    private string GetAttributeNameFromLambdaExpression(LambdaExpression expression)
-    {
-        if (expression.Body is MemberExpression memberExpression && memberExpression.Member is PropertyInfo propertyInfo)
-        {
-            return propertyInfo.Name;
-        }
-        throw new UnsupportedIndexImplementationException($"The LambdaExpression {expression} does not represent accessing a property on the entity");
     }
 }
